3fbc90227b8e04720f6f9eb17cde48652986febf
peter flanagan <sirflano@gmail.com>
2017-05-05 16:58:46 +0100

    End of test, did not get orbiting working

--- Assets/orbit.cs
+++ Assets/orbit.cs
@@ -15,13 +15,26 @@ public class orbit : steeringBehaviour {
     {
         Vector3 posRelToCentre = centre.transform.position - transform.position;
         float distance = posRelToCentre.magnitude;
-        float angle = Vector3.Angle(centre.transform.position, transform.position);
+        Vector3 ross = Vector3.Cross(centre.transform.position, transform.up);
+        ross += transform.position;
+        //Debug.Log(Vector3.Cross(centre.transform.position, transform.position));
+        Vector3 tempForward = Vector3.forward;
+        Vector3 angle = Vector3.Cross(centre.transform.position, transform.up);
+        angle *= Vector3.Dot(centre.transform.position, transform.position);
+        Debug.Log(angle);
+
+
         
 
-        Quaternion desiredAngle = Quaternion.Euler(0, (angle + 5) % 360, 0);
+        //Quaternion desiredAngle = Quaternion.Euler(0, (angle + 5) % 360, 0);
 
-        Vector3 targetPos = desiredAngle * centre.transform.position;
-        targetPos.Normalize();
+        //Vector3 tempVec = new Vector3(1, 0, 0);
+        //tempVec = desiredAngle * tempVec;
+        //Debug.Log(tempVec);
+        //tempVec *= distance;
+        /*
+        Vector3 targetPos = (desiredAngle * centre.transform.position) * distance;
+        
         Debug.Log(targetPos); 
         //float angle = Vector3.Angle
 
@@ -30,16 +43,17 @@ public class orbit : steeringBehaviour {
         transform.LookAt(centre.transform);
         Vector3 direction = transform.right;
         transform.LookAt(direction);
-        direction.Normalize();
-        direction *= maxSpeed;
-        direction += transform.position;
+        //direction.Normalize();
+        //direction *= maxSpeed;
+        direction += transform.position;*/
 
+        
         //Quaternion desiredAngle = Quaternion.Euler(0, (angle + 5) % 360, 0);
         //Vector3 targetPos = new Vector3(distance, 0, 0) + centre.transform.position;
         //targetPos = desiredAngle * targetPos;
         //Debug.Log(angle);
 
-        return boid.seekTarget(targetPos * distance);
+        return boid.seekTarget(ross);
     }
     // Update is called once per frame
     void Update () {
